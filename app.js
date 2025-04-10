e062241ead48ff422a6fd6f04966c2abe2e7706a
@chisanupong <chisanupong.limsakul@gmail.com>
2025-04-10 14:29:41 +0700

    app.js

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use((err, req, res, next) => {
     res.status(500).json({ status: false, message: "Internal Server Error" })
   })
 
-let PORT = process.env.PORT || 3000
-app.listen(PORT , () => 
-    console.log(`Server runnig on port ${PORT}`)
-)
+// let PORT = process.env.PORT || 3000
+// app.listen(PORT , () => 
+//     console.log(`Server runnig on port ${PORT}`)
+// )
