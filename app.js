48354c0b3fbfaf32e3c73afd49a872831df49fad
@chisanupong <chisanupong.limsakul@gmail.com>
2025-04-16 21:31:49 +0700

    +

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.use((err, req, res, next) => {
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
 module.exports = app;
